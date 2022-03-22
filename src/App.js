a2163d348fb22b2924822f6274ffaf4197dd8a0a
junior <jgj_junior@yahoo.com.br>
2022-03-22 19:49:32 -0300

    imagem e link da noticia

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     const fetchArtigos = async () => {
       try {
         const res = await fetch(
-          `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${process.env.REACT_APP_API_KEY}`
+          `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=brazil&api-key=${process.env.REACT_APP_API_KEY}`
         );
         const artigos = await res.json();
         // console.log(artigos.response.docs);
@@ -27,14 +27,20 @@ function App() {
           abstract,
           headline: { main },
           lead_paragraph,
+          multimedia,
+          web_url,
         } = artigo;
+        const imagem = `https://www.nytimes.com/${multimedia[0].url}`;
 
         return (
           <div key={_id}>
-            <h2>{main}</h2>
+            <a style={{ fontSize: 30 }} href={web_url}>
+              {main}
+            </a>
             <h4>{abstract}</h4>
             <p>{lead_paragraph}</p>
-            <br />
+            <img src={imagem} alt={imagem} width="300" />
+            <hr />
           </div>
         );
       })}
